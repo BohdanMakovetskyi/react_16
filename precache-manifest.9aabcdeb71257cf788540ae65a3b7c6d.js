b851581b5fb38c645bb5a54b78ea7bf3354c7bb7
Bohdan <bohdan.makovetskyi@gmail.com>
2020-03-31 23:22:36 +0300

    Updates

---
+++ precache-manifest.9aabcdeb71257cf788540ae65a3b7c6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49e3fa52f756e899f4796a84765bdf58",
+    "url": "/react_16/index.html"
+  },
+  {
+    "revision": "c14e442de4ca45e0d06e",
+    "url": "/react_16/static/css/main.b66eefe9.chunk.css"
+  },
+  {
+    "revision": "c374ffcea67aadbe5f12",
+    "url": "/react_16/static/js/2.fa522e3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_16/static/js/2.fa522e3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c14e442de4ca45e0d06e",
+    "url": "/react_16/static/js/main.2d672972.chunk.js"
+  },
+  {
+    "revision": "6ca72aebfd417db0b6fc",
+    "url": "/react_16/static/js/runtime-main.430e298d.js"
+  }
+]);
