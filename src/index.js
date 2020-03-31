19ddadfe97f33169ed47b32ab71588a49055d4ce
Bohdan <bohdan.makovetskyi@gmail.com>
2020-03-31 23:19:16 +0300

    finish

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Contacts from './Contacts';
+
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Contacts/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
